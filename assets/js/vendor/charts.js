043990c106a3124b84135c5e6f6983ec1c787898
naeemdeveloper <azharulislamnaeembd@gmail.com>
2023-03-12 09:35:56 +0600

    part-2 responsive done

--- assets/js/vendor/charts.js
+++ assets/js/vendor/charts.js
@@ -1,22 +1,19 @@
-window.onload = function () {
-
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        theme: "light2", // "light1", "light2", "dark1", "dark2"
-        data: [{        
-            type: "bar",  
-            showInLegend: true, 
-            legendMarkerColor: "grey",
-            dataPoints: [      
-                { y: 300878, label: "Marka C" },
-                { y: 266455,  label: "Marka B" },
-                { y: 169709,  label: "Marka A" },
-                { y: 158400,  label: "Elmex" },
-                { y: 142503,  label: "Colgate" },
-                { y: 101500, label: "Kategoria past" },
-            ]
+const ctx = document.getElementById('myChart');
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
         }]
-    });
-    chart.render();
-    
-}
+    },
+    options: {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+        }
+    }
+});
